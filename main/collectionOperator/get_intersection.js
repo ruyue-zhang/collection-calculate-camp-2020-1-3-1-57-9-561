305b27e4e5d5430aeaf049ccb342610cdf72acc4
lpdft <772346730@qq.com>
2020-01-06 23:38:18 +0800

    [ruyue] feat: spec/collectionOperator/get_intersection_spec.js

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -2,6 +2,9 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  return collection_b.filter(value=>{
+    return -1 !== collection_a.indexOf(value);
+  });
 }
 
 module.exports = get_intersection;
