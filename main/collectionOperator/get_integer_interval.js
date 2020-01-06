08188297bc94e96e426c95ef5f544d96b70e6a5f
lpdft <772346730@qq.com>
2020-01-06 23:34:38 +0800

    [ruyue] feat: spec/collectionOperator/get_integer_interval_spec

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -2,6 +2,21 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  let result = [];
+  if(number_a <= number_b) {
+    getIncrease(number_a, number_b, result);
+    return result;
+  }
+  else {
+    getIncrease(number_b, number_a, result);
+    return result.reverse();
+  }
+}
+
+function getIncrease(min,max,result) {
+  for(let i = min; i <= max; i++) {
+    result.push(i);
+  }
 }
 
 module.exports = get_integer_interval;
