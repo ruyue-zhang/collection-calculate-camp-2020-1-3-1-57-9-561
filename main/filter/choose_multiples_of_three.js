d6761bac5cd570d3dd0ab5ae336f701a9da9b29a
lpdft <772346730@qq.com>
2020-01-07 14:34:06 +0800

    [ruyue] feat: spec/filter/choose_multiples_of_three_spec

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
   //在这里写入代码
+  return collection.filter(value=> {
+    return value % 3 === 0;
+  });
 }
 
 module.exports = choose_multiples_of_three;
