0ffbe888cb5afd560430a5923624ccd3467b4239
lpdft <772346730@qq.com>
2020-01-07 17:21:19 +0800

    [ruyue] feat: spec/filter/grouping_count_spec

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function grouping_count(collection) {
-
   //在这里写入代码
+  let object = {};
+  collection.forEach(element => {
+    element in object ? object[element]++ : object[element] = 1;
+  });
+  return object;
 }
 
 module.exports = grouping_count;
