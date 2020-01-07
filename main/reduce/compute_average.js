83453237850d3682067d221032baea67da94ef8b
lpdft <772346730@qq.com>
2020-01-07 20:01:28 +0800

    [ruyue] feat: spec/reduce/compute_average_spec

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -2,6 +2,14 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  return collection.reduce((total,value,index)=>{
+    if(index === collection.length - 1) {
+      return (total + value) / collection.length;
+    }
+    else {
+      return total + value;
+    }
+  });
 }
 
 module.exports = compute_average;
