df063b7d901c47c7cd5060daf867369001c79d23
lpdft <772346730@qq.com>
2020-01-07 17:24:49 +0800

    [ruyue] feat: spec/filter/two_collections_practice_2_spec

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
   //在这里写入代码
+  return collection_a.filter(element=>{
+    return collection_b.indexOf(element) === -1;  
+  });
 }
 
 module.exports = choose_no_common_elements;
