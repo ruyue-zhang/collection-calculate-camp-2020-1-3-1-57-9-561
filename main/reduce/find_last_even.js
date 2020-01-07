2318a9ad21ee29422931cb8d59692f44b2db5942
lpdft <772346730@qq.com>
2020-01-07 20:36:03 +0800

    [ruyue] feat: spec/reduce/find_last_even_spec

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -2,6 +2,14 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  collection = chooseEven(collection);
+  return collection[collection.length - 1];
+}
+
+function chooseEven(collection) {
+  return collection.filter(value=> {
+    return value % 2 === 0;
+  });
 }
 
 module.exports = find_last_even;
