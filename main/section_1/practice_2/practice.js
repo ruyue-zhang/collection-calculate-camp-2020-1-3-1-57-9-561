3a79518264387adbf806df8af5c3b25e2f05ef37
lpdft <772346730@qq.com>
2020-01-07 20:57:46 +0800

    [ruyue] feat: spec/section_1/practice_2_spec

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,14 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.filter(element_a=>{
+    let isTrue = false;
+    collection_b.forEach(element_b=>{
+      if(element_b.indexOf(element_a) !== -1) {
+        isTrue = true;
+      }  
+    });
+    return isTrue;
+  });
 }
 
 module.exports = collect_same_elements;
