02911fdf84ab056f9c393e7a12dbc9122732f43b
lpdft <772346730@qq.com>
2020-01-07 21:22:53 +0800

    [ruyue] feat: spec/section_1/practice_4_spec

--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,5 +1,10 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  collection_a = collection_a.filter(element_a=>{
+    return object_b.value.indexOf(element_a.key) !== -1;
+  });
+  return collection_a.map(element=>element.key);
+
 }
 
 module.exports = collect_same_elements;
