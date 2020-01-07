e66c2fe82616499d649a049e238ac805162f2ee8
lpdft <772346730@qq.com>
2020-01-07 18:37:36 +0800

    [ruyue] feat: spec/map/number_map_to_word

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,12 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return numberToLetter(collection);
 };
+
+function numberToLetter(collection) {
+  return collection.map(element =>{
+      return String.fromCharCode(element + 96);
+  });
+}
 
 module.exports = number_map_to_word;
