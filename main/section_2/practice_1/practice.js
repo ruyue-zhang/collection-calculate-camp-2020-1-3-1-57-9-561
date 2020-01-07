60c2e462c573d5ff54d8be58368d68f898c5aca6
lpdft <772346730@qq.com>
2020-01-07 23:01:09 +0800

    [ruyue] feat: spec/section_2/practice_1_spec

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,28 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  let countObject = groupingCount(collection);
+  return objectToArrObj(countObject);
 }
+
+function groupingCount(collection) {
+  let object = {};
+  collection.forEach(element => {
+    element in object ? object[element]++ : object[element] = 1;
+  });
+  return object;
+}
+
+function objectToArrObj(countObject) {
+  let result = [];
+  for(value in countObject) {
+    result.push({
+      key: value,
+      count: countObject[value],
+    });
+  }
+  return result;
+}
+
+
 
 module.exports = count_same_elements;
