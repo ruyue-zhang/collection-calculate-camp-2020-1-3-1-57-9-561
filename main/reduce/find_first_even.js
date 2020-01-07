e4626fb592d7a01dace14fe3a5de33b053ecfe27
lpdft <772346730@qq.com>
2020-01-07 20:35:06 +0800

    [ruyue] feat: spec/reduce/find_first_even_spec

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -2,6 +2,14 @@
 
 function find_first_even(collection) {
   //在这里写入代码
+  collection = chooseEven(collection);
+  return collection[0];
+}
+
+function chooseEven(collection) {
+  return collection.filter(value=> {
+    return value % 2 === 0;
+  });
 }
 
 module.exports = find_first_even;
