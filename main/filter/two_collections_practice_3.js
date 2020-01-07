84bb63f0073e3160d577e9ffc276c221e43c7ac6
lpdft <772346730@qq.com>
2020-01-07 17:58:59 +0800

    [ruyue] feat: spec/filter/two_collections_practice_3_spec

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
   //在这里写入代码
+  return collection_a.filter(element_a=>{
+    let isTrue = false;
+    collection_b.forEach(element_b=>{
+      if(0 === element_a % element_b) {
+        isTrue = true;
+      }
+    });
+    return isTrue;
+  });
 }
 
 module.exports = choose_divisible_integer;
