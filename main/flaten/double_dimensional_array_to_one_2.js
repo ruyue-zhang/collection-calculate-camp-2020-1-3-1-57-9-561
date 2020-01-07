a93a038cac71c041d541abdb7668488e4f8c95da
lpdft <772346730@qq.com>
2020-01-07 18:11:23 +0800

    [ruyue] feat: spec/flaten/double_dimensional_array_to_one_2_spec

--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  let result = flatten(collection);
+  return chooseNoRepeatNumber(result);
+}
+
+const flatten = arr => arr.reduce((pre, val) => pre.concat(Array.isArray(val) ? flatten(val) : val), []);
+
+function chooseNoRepeatNumber(collection) {
+  return collection.filter((element,index)=>{
+    return collection.indexOf(element) === index;  
+  });
 }
 
 module.exports = double_to_one;
