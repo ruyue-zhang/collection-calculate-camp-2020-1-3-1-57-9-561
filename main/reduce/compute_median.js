dfdb31c63ee5046333d297c90fc0eefcc29c9af0
lpdft <772346730@qq.com>
2020-01-07 20:22:00 +0800

    [ruyue] feat: spec/reduce/compute_median_spec

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -2,6 +2,19 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  arraySortAsc(collection);
+  return getMedianNumber(collection);
+}
+
+function arraySortAsc(collection) {
+  collection.sort((a,b)=>{
+    return a - b;
+  });
+}
+
+function getMedianNumber(collection) {
+  var len = collection.length;
+  return  len % 2 ? collection[Math.floor(len / 2)] : (collection[len / 2] + collection[len / 2 - 1]) / 2;
 }
 
 module.exports = compute_median;
