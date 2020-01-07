b85ad30aa21bb553f2e51727cc6bfbd4ff5748eb
lpdft <772346730@qq.com>
2020-01-07 20:50:10 +0800

    [ruyue] feat: spec/ruduce/split_to_zero_spec

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -2,6 +2,12 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var result = [number];
+  while (number  > 0) {
+    number = Math.round((number - interval) * 10) / 10;
+    result.push(number);
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
