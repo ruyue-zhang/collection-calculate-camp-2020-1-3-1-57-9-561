cf2745f10e3aacfb01abaaca22f8ae01f178c8a7
lpdft <772346730@qq.com>
2020-01-07 20:28:15 +0800

    [ruyue] feat: spec/reduce/compute_sum_spec

--- main/reduce/compute_sum.js
+++ main/reduce/compute_sum.js
@@ -2,6 +2,9 @@
 
 function calculate_elements_sum(collection) {
   //在这里写入代码
+  return collection.reduce((total, currentValue)=>{
+    return total + currentValue;
+  })
 }
 
 module.exports = calculate_elements_sum;
