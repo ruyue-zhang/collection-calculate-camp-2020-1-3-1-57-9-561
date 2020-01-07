a4fa44c23d1b91d2ecf13a29ccbc4c7fd5597795
lpdft <772346730@qq.com>
2020-01-07 14:39:42 +0800

    [ruyue] feat: spec/filter/choose_no_repeat_number_spec

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
   //在这里写入代码
+  return collection.filter((element,index)=>{
+    return collection.indexOf(element) === index;  
+  });
 }
-
 module.exports = choose_no_repeat_number;
