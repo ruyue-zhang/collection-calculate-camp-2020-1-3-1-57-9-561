2deb46266b7871764389f0e793f1879ad0b8bec9
lpdft <772346730@qq.com>
2020-01-07 14:26:15 +0800

    [ruyue] feat: spec/collectionOperator/get_letter_interval_spec

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -2,6 +2,27 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  let result = [];
+  if(number_a < number_b) {
+    getIncrease(number_a, number_b,result);
+    return numberToLetter(result);
+  }
+  else {
+    getIncrease(number_b, number_a,result);
+    return numberToLetter(result).reverse();
+  }
+}
+
+function getIncrease(min,max,result) {
+  for(let i = min; i <= max; i++) {
+    result.push(i);
+  }
+}
+
+function numberToLetter(collection) {
+  return collection.map(element =>{
+      return String.fromCharCode(element + 96);
+  });
 }
 
 module.exports = get_letter_interval;
