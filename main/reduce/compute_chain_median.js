3a40fedd9d1f6d205e8ad71c382cb94f82507b0f
lpdft <772346730@qq.com>
2020-01-07 20:19:33 +0800

    [ruyue] feat: spec/reduce/compute_chain_median_spec

--- main/reduce/compute_chain_median.js
+++ main/reduce/compute_chain_median.js
@@ -2,6 +2,26 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
+  collection = stringToNumberArray(collection);
+  arraySortAsc(collection);
+  return getMedianNumber(collection);
+}
+
+function stringToNumberArray(collection) {
+  return collection.split('->').map(value=>{
+    return parseInt(value);
+  });
+}
+
+function arraySortAsc(collection) {
+  collection.sort((a,b)=>{
+    return a - b;
+  });
+}
+
+function getMedianNumber(collection) {
+  var len = collection.length;
+  return  len % 2 ? collection[Math.floor(len / 2)] : (collection[len / 2] + collection[len / 2 - 1]) / 2;
 }
 
 module.exports = compute_chain_median;
