29bfa27b957c1613b131bc02c714c2c9614d55b3
lpdft <772346730@qq.com>
2020-01-08 11:27:49 +0800

    [ruyue] feat：spec/section_3/practice_1_spec

--- main/section_3/practice_1/practice.js
+++ main/section_3/practice_1/practice.js
@@ -1,5 +1,13 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  object_b.value.forEach(value_b=> {
+    collection_a.forEach(value_a=> {
+      if(value_a.key === value_b) {
+        value_a.count--;
+      }
+    });
+  });
+  return collection_a;
 }
 
 module.exports = create_updated_collection;
