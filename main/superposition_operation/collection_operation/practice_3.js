9bccec20f08ae7d4404e2edffedc80e35a605e41
lpdft <772346730@qq.com>
2020-01-08 13:28:56 +0800

    [ruyue] feat：spec/superposition_operation/collection_operation/practice_3_spec

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
   //在这里写入代码
+  collection = collectAllOdd(collection);
+  return multiThreeAddFiveGetTotal(collection);
+}
+
+function collectAllOdd(collection) {
+  return collection.filter(value=>{return value % 2});
+}
+
+function multiThreeAddFiveGetTotal(collection) {
+  return collection.reduce((total, element)=>{
+    return total + element * 3 + 5;
+  }, 0);
 }
 
 module.exports = hybrid_operation_to_uneven;
