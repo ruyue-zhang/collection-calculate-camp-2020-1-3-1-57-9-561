748bd8bbda848a67184b2cf72e7c832bb45a73b2
lpdft <772346730@qq.com>
2020-01-08 14:52:05 +0800

    [ruyue] feat: spec/superposition_operation/own_elements_operation/subscript_is_even/calculate_average_spec

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,25 @@
 'use strict';
 var calculate_average = function(collection){
-
+  collection = getEvenElement(collection);
+  console.log(collection);
+  return computeAverage(collection);
 };
+
+function getEvenElement(collection) {
+  return collection.filter((currentValue, index)=>{
+    return index % 2;
+  })
+}
+
+function computeAverage(collection) {
+  return collection.reduce((total,value,index)=>{
+    if(index === collection.length - 1) {
+      return (total + value) / collection.length;
+    }
+    else {
+      return total + value;
+    }
+  });
+}
+
 module.exports = calculate_average;
