e5f0d32b35c276a579d9ff7bcfe3442fa9bb83cb
lpdft <772346730@qq.com>
2020-01-08 13:33:03 +0800

    [ruyue] feat: spec/superposition_operation/interval_operation/amount_even_spec

--- main/superposition_operation/interval_operation/amount_even.js
+++ main/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function amount_even(collection) {
-
   //在这里写入代码
+  collection = collectAllEven(collection);
+  return getArrayTotal(collection);
+}
+
+function collectAllEven(collection) {
+  return collection.filter(value=>{return !(value % 2)});
+}
+
+function getArrayTotal(collection) {
+  return collection.reduce((total, value)=>{
+    return total + value;
+  }, 0);
 }
 
 module.exports = amount_even;
