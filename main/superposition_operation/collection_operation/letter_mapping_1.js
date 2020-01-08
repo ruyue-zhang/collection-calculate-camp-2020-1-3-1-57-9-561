6af10970b6472655730f2c604462c4ca3ed659ad
lpdft <772346730@qq.com>
2020-01-08 12:03:54 +0800

    [ruyue] feat：spec/superposition_operation/collection_operation/letter_mapping_1_spec

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function even_to_letter(collection) {
-
   //在这里写入代码
+  collection = collectAllEven(collection);
+  return numberToLetter(collection);
+}
+
+function collectAllEven(collection) {
+  return collection.filter(value=>{return !(value % 2)});
+}
+
+function numberToLetter(collection) {
+  return collection.map(element =>{
+      return String.fromCharCode(element + 96);
+  });
 }
 
 module.exports = even_to_letter;
