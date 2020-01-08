488fc349685b43471879027d227566e858dddaa1
lpdft <772346730@qq.com>
2020-01-08 13:43:22 +0800

    [ruyue] feat: spec/superposition_operation/interval_operation/median_to_letter_spec

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,8 +1,25 @@
 'use strict';
 
 function median_to_letter(collection) {
-
   //在这里写入代码
+  let median = getMedianNumber(collection);
+  return numberToLetter(Math.ceil(median));
 }
+
+function getMedianNumber(collection) {
+  var len = collection.length;
+  return  len % 2 ? collection[Math.floor(len / 2)] : (collection[len / 2] + collection[len / 2 - 1]) / 2;
+}
+
+function numberToLetter(median) {
+  if(median <= 26) {
+    return String.fromCharCode(median + 96);
+  }
+  else {
+    let count = Math.ceil(median / 26) - 1;
+    return String.fromCharCode(count + 96) + String.fromCharCode(median - 26 * count + 96);
+  }
+}
+
 
 module.exports = median_to_letter;
