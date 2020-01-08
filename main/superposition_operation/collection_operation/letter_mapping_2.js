3775890e8e674f3ba978fd3cabcd30cd7a16414a
lpdft <772346730@qq.com>
2020-01-08 13:10:38 +0800

    [ruyue] feat：spec/superposition_operation/collection_operation/letter_mapping_2_spec

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function average_to_letter(collection) {
-
   //在这里写入代码
+  let average = computeAverage(collection);
+  return numberToLetter(Math.ceil(average));
+}
+
+function computeAverage(collection) {
+  return collection.reduce((total,value,index)=>{
+    if(index === collection.length - 1) {
+      return (total + value) / collection.length;
+    } else {
+      return total + value;
+    }
+  });
+}
+
+function numberToLetter(average) {
+  return String.fromCharCode(average + 96);
 }
 
 module.exports = average_to_letter;
