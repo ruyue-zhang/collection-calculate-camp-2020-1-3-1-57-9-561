66e979f9236201b6e0453115362fd41b42acc86a
lpdft <772346730@qq.com>
2020-01-08 17:08:42 +0800

    [ruyue] feat: spec/superposition_operation/own_elements_operation/even_asc_odd_desc_spec

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,21 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
 
+var even_asc_odd_desc = function(collection){
+  let even = collectAllEvenAsc(collection);
+  let odd = collectAllOddDes(collection);
+  return even.concat(odd);
 };
+
+function collectAllEvenAsc(collection) {
+  let even = collection.filter(value=>{return !(value % 2)});
+  even.sort((a,b)=>{return a - b;});
+  return even;
+}
+
+function collectAllOddDes(collection) {
+  let odd = collection.filter(value=>{return value % 2});
+  odd.sort((a,b)=>{return b - a});
+  return odd;
+}
+
 module.exports = even_asc_odd_desc;
