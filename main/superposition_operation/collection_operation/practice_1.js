ee30e8f34a41610144cef18f2b9e73f355a61024
lpdft <772346730@qq.com>
2020-01-08 13:14:19 +0800

    [ruyue] feat：spec/superposition_operation/collection_operation/practice_1_spec

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function hybrid_operation(collection) {
-
   //在这里写入代码
+  return collection.reduce((total, currentValue)=>{
+    return total + currentValue * 3 + 2;
+  } ,0);
+
 }
 
 module.exports = hybrid_operation;
