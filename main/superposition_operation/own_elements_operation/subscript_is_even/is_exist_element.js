886d2b3af74b8c4fd417004621fc8837f2b4b8f5
lpdft <772346730@qq.com>
2020-01-08 16:32:02 +0800

    [ruyue] feat: spec/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element_spec

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,13 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  collection = getEvenElement(collection);
+  return -1 !== collection.indexOf(element);
 };
+
+function getEvenElement(collection) {
+  return collection.filter((currentValue, index)=>{
+    return !(index % 2);
+  })
+}
+
 module.exports = is_exist_element;
