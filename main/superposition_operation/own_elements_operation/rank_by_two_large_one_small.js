364e7fe56b23606839df2648185ab9e1cd2c316b
lpdft <772346730@qq.com>
2020-01-08 17:21:01 +0800

    [ruyue] feat: spec/superposition_operation/own_elements_operation/rank_by_two_large_one_small_spec

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,34 @@
 'use strict';
+
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  rankAsc(collection);
+  return sortByTwoLargeOneSmall(collection);
 }
+
+function rankAsc(collection) {
+  collection.sort((a, b)=>{
+    return a - b;
+  });
+}
+
+function sortByTwoLargeOneSmall(collection) {
+  var result = [];
+  var len = collection.length;
+  for(var i = 0; i < len; i += 3) {
+    if(i + 2 < len) {
+      result.push(collection[i + 1]);
+      result.push(collection[i + 2]);
+      result.push(collection[i]);
+    }
+    else if(i + 1 < len){
+      result.push(collection[i]);
+      result.push(collection[i + 1]);
+    }
+    else {
+      result.push(collection[i]);
+    } 
+  }
+  return result;
+}
+
 module.exports = rank_by_two_large_one_small;
