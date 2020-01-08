5c332ac5d4504a141d2c0443d7d27a9bd267bc63
lpdft <772346730@qq.com>
2020-01-08 13:36:30 +0800

    [ruyue] feat: spec/superposition_operation/interval_operation/average_uneven_spec

--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,24 @@
 'use strict';
 
 function average_uneven(collection) {
-
   //在这里写入代码
+  collection = collectAllOdd(collection);
+  return getAverage(collection);
+}
+
+function collectAllOdd(collection) {
+  return collection.filter(value=>{return value % 2});
+}
+
+function getAverage(collection) {
+  return collection.reduce((total, value, index)=>{
+    if(index === collection.length - 1) {
+      return (total + value) / collection.length;
+    }
+    else {
+      return total + value;
+    }
+  });
 }
 
 module.exports = average_uneven;
